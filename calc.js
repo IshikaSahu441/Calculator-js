906d8549e2905ca43755fcaa8c370d184815fa4e
IshikaSahu441 <ishika.sahu22@st.niituniversity.in>
2025-01-15 18:31:22 +0530

    calc sub

--- calc.js
+++ calc.js
@@ -1,11 +1,13 @@
-import {subtraction} from './subtraction.js';
+import { subtraction } from './subtraction.js';
 
-const num1 = prompt("enter first number: ")
-const num2 = prompt("enter second number: ")
-
-const ch = prompt("enter you choice: 1,2,3,4")
+const num1 = Number(prompt("Enter first number: "));
+const num2 = Number(prompt("Enter second number: "));
+const ch = Number(prompt("Enter your choice: 1, 2, 3, 4"));
 
 switch (ch) {
-    case 1:
-        console.log(subtraction(num1,num2))
+  case 1:
+    console.log(subtraction(num1, num2));
+    break;
+  default:
+    console.log("Invalid choice");
 }
